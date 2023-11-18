8add5beed2bb99095209aa974f6a790011b50015
bronese <nghanwei99@live.com>
2023-11-18 03:13:56 -0500

    uhhh i think this works???/

--- DDNS-update.py
+++ DDNS-update.py
@@ -13,6 +13,7 @@ zone_id = os.getenv("ZONEID")
 record_id = os.getenv("RECORDID")
 update_interval = os.getenv("UPDATEINTERVAL")
 ttl = os.getenv("TTL")
+selecteditem = os.getenv("SELECTEDITEM")
 
 current_time=(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
@@ -22,6 +23,18 @@ def check_env(env_name):
     if not value:
         print(f"{env_name} is empty.")
 
+# Get DNS records
+def get_dns_record(email, token, zone_id):
+    headers = get_headers(email, token)
+    response = requests.get(
+        f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records",
+        headers=headers
+    )
+    dns_records = response.json()["result"]
+    if not dns_records:
+        print("No DNS records found.")
+    else:
+        return dns_records
 
 # Build headers
 def get_headers(email, token):
@@ -51,8 +64,25 @@ def get_ip(ip_url=None):
     response = requests.get(ip_url)
     return response.text.rstrip()
 
+# Get generated record ID
+def get_generated_record_id(dns_record, selecteditem):
+    filtered_payload = []
+    counter = 0
+
+    while counter < len(dns_record):
+        if dns_record[counter]['type'] == 'A':
+            filtered_payload.append(dns_record[counter])
+            break
+
+    if len(filtered_payload) > 1:
+        dns_record_id = filtered_payload[selecteditem]
+        return dns_record_id['id']
+    else:
+        dns_record_id = filtered_payload[0]
+        return dns_record_id['id']
+
 # Main function
-def main(domain, name, record_type, ip_url, email, token, zone_id, record_id):
+def main(domain, name, record_type, ip_url, email, token, zone_id, final_record_id):
     # Get IP
     new_ip = get_ip(ip_url)
 
@@ -72,7 +102,7 @@ def main(domain, name, record_type, ip_url, email, token, zone_id, record_id):
     
     # Send the PUT request
     response = requests.put(
-        f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}",
+        f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{final_record_id}",
         headers=headers,
         json=data,
     )
@@ -100,12 +130,20 @@ check_env("RECORDID")
 # Verify authentication on boot
 verify_auth(email, token)
 
-# IP update schedule
+# Set current IP
 current_ip=get_ip()
 
-while True:
+#DNS record ID checker
+dns_record = get_dns_record(email, token, zone_id)
+if selecteditem!=None:
+     final_record_id = get_generated_record_id(dns_record, selecteditem)
+else:
+     final_record_id=record_id
+
+# Main loop
+while current_ip != None:
     new_ip = get_ip(ip_url)
-    main(domain, name, record_type, ip_url, email, token, zone_id, record_id)
+    main(domain, name, record_type, ip_url, email, token, zone_id, final_record_id)
     print(f"Updated IP from {current_ip} to {new_ip} at {current_time}.")
     current_ip = new_ip
     wait_time = int(update_interval if update_interval is not None else 300)
