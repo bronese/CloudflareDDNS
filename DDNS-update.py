4e786f02163e72b8b0bc9cc6944fd153a0e7f0a3
bronese <nghanwei99@live.com>
2023-11-18 04:10:24 -0500

    allowed selected item to be int but also None

--- DDNS-update.py
+++ DDNS-update.py
@@ -13,7 +13,7 @@ zone_id = os.getenv("ZONEID")
 record_id = os.getenv("RECORDID")
 update_interval = os.getenv("UPDATEINTERVAL")
 ttl = os.getenv("TTL")
-selecteditem = int(os.getenv("SELECTEDITEM"))
+selecteditem = int(os.getenv("SELECTEDITEM")) if os.getenv("SELECTEDITEM") else None
 
 current_time=(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
